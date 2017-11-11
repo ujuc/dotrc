41b7027d9e6c7f98e09380a96b7b91a802432eba
Sungjin Kang <ujuc@ujuc.kr>
2017-11-11 12:35:55 +0900

    Add command on zsh
    * Add cleanup

--- zsh/aliass.zsh
+++ zsh/aliass.zsh
@@ -15,6 +15,7 @@ if [[ $(uname -s) == "Darwin" ]]; then
     alias cask="brew cask"
     alias update="brew update; brew upgrade"
     alias upip="pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U"
+    alias cleanup="brew cleanup"
 else
     DISTRO_ID=$(python -c "import distro; print(distro.id())")
 
@@ -24,7 +25,7 @@ else
         alias search="sudo apt search"
         alias info="sudo apt show"
         alias update="sudo apt update; sudo apt full-upgrade"
-        alias clean="sudo apt autoremove -y"
+        alias cleanup="sudo apt autoremove -y"
         alias add_repo="sudo add-apt-repository"
         alias rm_reop="sudo add-apt-repository -r"
     elif [[ $DISTRO_ID == "manjaro" ]]; then
@@ -47,6 +48,7 @@ $fg_bold[red]* info: $fg_bold[white]brew info
 $fg_bold[red]* blist: $fg_bold[white]brew list
 $fg_bold[red]* update: $fg_bold[white]brew update; brew upgrade
 $fg_bold[red]* upip: $fg_bold[white]pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U
+$fg_bold[red]* cleanup: $fg_bold[white]brew clenaup
 
 $fg_bold[blue]# Ubuntu
 $fg_bold[red]* uupdate: $fg_bold[white]sudo apt-get update; sudo apt-get dist-upgrade
@@ -54,7 +56,7 @@ $fg_bold[red]* install: $fg_bold[white]sudo apt-get install -y
 $fg_bold[red]* uninstall: $fg_bold[white]sudo apt-get purge -y
 $fg_bold[red]* search: $fg_bold[white]sudo apt-cache search
 $fg_bold[red]* info: $fg_bold[white]sudo apt-cahce show
-$fg_bold[red]* clean: $fg_bold[white]sudo apt-get autoremove -y
+$fg_bold[red]* cleanup: $fg_bold[white]sudo apt-get autoremove -y
 $fg_bold[red]* update: $fg_bold[white]sudo apt-get update; sudo apt-get dist-upgrade
 $fg_bold[red]* add_repo: $fg_bold[white]sudo add-apt-repository
 $fg_bold[red]* rm_reop: $fg_bold[white]sudo add-apt-repository -r
