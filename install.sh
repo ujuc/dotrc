67eac170665ee1930a66bb394985009c15612954
Thomas Sungjin Kang <ujuc@ujuc.kr>
2018-12-04 01:01:54 +0900

    Renew install script

--- install.sh
+++ install.sh
@@ -3,136 +3,44 @@
 #############################
 #   System Setting          #
 #   Sungjin (ujuc@ujuc.kr)  #
+#   Version : 2.0           #
 #############################
 
 BASE=$(pwd)
-FUNC=$1
 
-function printMessage() {
-    echo -e "\e[1;37m# $1\033[0m"
+function setting_mac() {
+
 }
 
-function installSystemPackage() {
-    printMessage "\nInstall defualt"
+function setting_ubuntu() {
 
-    linux=`cat /etc/*-release | grep '_ID'`
+}
 
-    if [ $(uname -s) = "Darwin" ]; then
-        [ -z "$(which brew)" ] && ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-        [ -z "$(which brew-cask)" ] && brew install caskroom/cask/brew-cask
-        brew install tig tmux python python3 zsh git
-        brew install vim --with-python3 --with-override-system-vi
-        brew cask install caskroom/fonts/font-hack
-    elif [ $(uname -s) = "Linux" ]; then
-        if [[ $linux == *ManjaroLinux* ]]; then
-            yaourt -S vim tig tmux zsh openssh
-        elif [[ $linux == *Ubuntu* ]]; then
-            sudo apt install -y vim tig tmux zsh
+function setting_redhat() {
+
+}
+
+function setting_arch() {
+
+}
+
+function bootstrap() {
+    # os check
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        setting_mac
+    elif [[ "$OSTYPE" == "linux-gnu" ]]; then
+        name=`cat /etc/*-release | grep '_ID'`
+
+        if [[ "$name" == *"Ubuntu"* ]]; then
+            setting_ubuntu
+        else
+            echo "Not checked"
         fi
+    else
+        echo "Not checked"
     fi
 }
 
-function settingTmux() {
-    printMessage "\nSetting tmux"
-    ln -sf $BASE/tmux.conf ~/.tmux.conf
-    tmux source-file ~/.tmux.conf
-    ln -sf $BASE/tigrc ~/.tigrc
-}
-
-function settingSubmodule () {
-    git submodule init
-    git submodule update
-}
-
-function settingZsh() {
-    printMessage "\nSetting zsh"
-
-    # zshrc setting
-    sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-
-    mkdir -p ~/.oh-my-zsh/custom/themes
-    # custom zshrc themes
-    # spaceship theme
-    #ln -sf $BASE/spaceship-zsh-theme/spaceship.zsh ~/.oh-my-zsh/custom/themes/spaceship.zsh-theme
-    # Plowerleve 9k
-    ln -sf $BASE/powerlevel9k/powerlevel9k.zsh-theme ~/.oh-my-zsh/custom/themes/powerlevel9k.zsh-theme
-
-    ln -sf $BASE/zshrc ~/.zshrc
-    #chsh -s /bin/zsh
-
-    # Setting powerline font
-    printMessage "\nPowerline Font"
-    bash $BASE/fonts/install.sh
-
-    # zsh-syntax-highlighting setting
-    mkdir -p ~/.oh-my-zsh/custom/plugins
-    git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-
-    source ~/.zshrc
-}
-
-function settingVim() {
-    # Link plug
-    mkdir -p ~/.vim/autoload
-    mkdir ~/.vim/bundle
-    mkdir ~/.vim/vimundo
-    ln -sf $BASE/vim-plug/plug.vim ~/.vim/autoload/plug.vim
-
-    # install amix/vimrc
-    git clone https://github.com/amix/vimrc.git ~/.vim_runtime
-    sh ~/.vim_runtime/install_awesome_vimrc.sh
-
-    # Bundle
-    #sudo pip install flake8 flake8-docstings
-    #sudo gem install reek
-    ln -sf $BASE/vimrcs ~/.vim/vimrcs
-    ln -sf $BASE/vimrc ~/.vimrc
-    #ln -sf /usr/local/bin/vim /usr/local/bin/vi
-
-    # linked theme
-    mkdir -p ~/.vim/colors
-    ln -sf ~/.vim/plugged/sourcerer.vim/colors/sourcerer.vim ~/.vim/colors/sourcerer.vim
-
-    # Install vim theme
-    ln -sf $BASE/vim/autoload/airline ~/.vim/autoload
-    git clone https://github.com/kristijanhusak/vim-hybrid-material.git $HOME/.vim/bundle/vim-hybrid-material
-
-    # Install YCM
-    git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/YouCompleteMe
-    cd ~/.vim/bundle
-    git submodule update --init --recursive
-    ~/.vim/bundle/YouCompleteMe/install.py --all
-
-    # Install Plugins
-    vi +PlugInstall +qall
-}
-
-case $FUNC in 
-    "all")
-        installSystemPackage
-        settingSubmodule
-        settingTmux
-        settingZsh
-        settingVim
-        ;;
-    "install")
-        installSystemPackage
-        ;;
-    "submodule")
-        settingSubmodule
-        ;;
-    "zsh")
-        settingZsh
-        ;;
-    "vim")
-        settingVim
-        ;;
-    "tmux")
-        settingTmux
-        ;;
-   * )
-        exit
-        ;;
-esac
+bootstrap
 
 #/* vim: noai:ts=4:sw=4
