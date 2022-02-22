4b6f27cc41f605259767102ee0b5044ba12792b9
sungjin.kang.sre <sungjin.kang_b@kurlycorp.com>
2022-02-22 16:16:01 +0900

    feat: fix zplug
    - 작동안되던 zplug 수정

--- zsh/zplug.zsh
+++ zsh/zplug.zsh
@@ -1,7 +1,7 @@
 export ZPLUG_HOME=/usr/local/opt/zplug
 source $ZPLUG_HOME/init.zsh
 
-zplug "zsh-users/zsh-syntax-highlighting", as:plugin
+zplug "zsh-users/zsh-syntax-highlighting", defer:2, as:plugin
 zplug "zsh-users/zsh-autosuggestions", as:plugin
 
 zplug load
--- zshrc
+++ zshrc
@@ -5,9 +5,6 @@ export DOTRC_ZSH="${DOTRC}/zsh"
 eval "$(starship init zsh)"
 
 # User configuration
-if (( $+commands[zplug] )); then
-  source $DOTRC_ZSH/zplug.zsh
-fi
-
+source $DOTRC_ZSH/zplug.zsh
 source $DOTRC_ZSH/aliases.zsh
 source $DOTRC_ZSH/env.zsh
